64ba091c55cd913bbd63d3fa41e3fc82b473de9a
ikostan <igorkostan@gmail.com>
2018-01-06 21:06:39 -0800

    HelloWorld app -> comments added

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
-
+    /**
+     * My first JAVA app
+     * Console app that prints "Hello World!" text
+     * */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        System.out.println("Hello World!"); // This line prints "Hello World!" text
     }
 }
