6bf7d0a103141082fa1d0fff120ab53894fbce47
ikostan <igorkostan@gmail.com>
2018-01-06 22:37:22 -0800

    Primitives -> working with variables

--- Primitives/src/com/company/primitives/Main.java
+++ Primitives/src/com/company/primitives/Main.java
@@ -3,6 +3,14 @@ package com.company.primitives;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("Variables, Data Types, and Math Operators\n");
+
+        //Local primitive variables
+        int dataValue = 100;
+        int myVar;
+        myVar = 10;
+
+        System.out.println(String.format("dataValue: {%d}, myVar: {%d}", dataValue, myVar));
     }
 }
