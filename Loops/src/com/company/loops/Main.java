08a8b77f254fe8f825ddee66708281529720491a
ikostan <igorkostan@gmail.com>
2018-01-07 22:21:18 -0800

    Loops

---
+++ Loops/src/com/company/loops/Main.java
@@ -0,0 +1,14 @@
+package com.company.loops;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+        while (i < 5){
+
+            System.out.println(String.format("%d", i));
+            i++;
+        }
+    }
+}
