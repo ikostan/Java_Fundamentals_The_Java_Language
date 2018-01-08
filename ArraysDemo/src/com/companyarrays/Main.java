a4330f010f4c386968c221e9ce5e282aac7a2f24
ikostan <igorkostan@gmail.com>
2018-01-07 22:31:22 -0800

    ArraysDemo

--- ArraysDemo/src/com/companyarrays/Main.java
+++ ArraysDemo/src/com/companyarrays/Main.java
@@ -3,6 +3,12 @@ package com.companyarrays;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Working with arrays
+
+        int[] intArr = {0,1,2,3,4,5,6,7,8,9};
+        for (int i : intArr){
+
+         System.out.print(i + ", ");
+        }
     }
 }
