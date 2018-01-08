33761dfe8bc7af61f13df87e7ff9348dae340f17
ikostan <igorkostan@gmail.com>
2018-01-07 22:40:33 -0800

    Conditions -> Switch statement

--- Conditions/src/com/company/conditions/Main.java
+++ Conditions/src/com/company/conditions/Main.java
@@ -44,5 +44,20 @@ public class Main {
 
             System.out.println(String.format("%d is not equal to %d", a, b));
         }
+
+        int num = 1234;
+
+        switch (num){
+
+            case 1:
+                System.out.println("The number is 1");
+                break;
+            case 2:
+                System.out.println("The number is 2");
+                break;
+                default:
+                    System.out.println("The number is not 1 or 2");
+                    break;
+        }
     }
 }
