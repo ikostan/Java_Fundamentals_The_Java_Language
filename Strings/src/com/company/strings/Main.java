edec71f7cf49518a01f0a2322894257b25aa26c2
ikostan <igorkostan@gmail.com>
2018-01-10 10:45:53 -0800

    Strings - string equality

--- Strings/src/com/company/strings/Main.java
+++ Strings/src/com/company/strings/Main.java
@@ -3,6 +3,32 @@ package com.company.strings;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        testEquality();
+    }
+
+    private static void testEquality(){
+
+        String s0 = "Hello";
+        String s1 = " World!";
+        String s2 = s0 + s1;
+        String s3 = "Hello World!";
+
+        //System.out.println(s2);
+        //System.out.println(s3);
+
+        if(s2.equals(s3)){
+            System.out.println("Equal");
+        }
+        else{
+            System.out.println("Not equal");
+        }
+
+        if(s2 == s3){
+            System.out.println("Equal");
+        }
+        else{
+            System.out.println("Not equal");
+        }
     }
 }
