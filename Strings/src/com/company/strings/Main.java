d9517660b5a7ce6f7cb877f3eef911dcc44e0fc6
ikostan <igorkostan@gmail.com>
2018-01-10 10:50:16 -0800

    Strings - string equality

--- Strings/src/com/company/strings/Main.java
+++ Strings/src/com/company/strings/Main.java
@@ -13,6 +13,8 @@ public class Main {
         String s1 = " World!";
         String s2 = s0 + s1;
         String s3 = "Hello World!";
+        String s4 = s2.intern();
+        String s5 = s2.intern();
 
         //System.out.println(s2);
         //System.out.println(s3);
@@ -25,6 +27,13 @@ public class Main {
         }
 
         if(s2 == s3){
+            System.out.println("Equal");
+        }
+        else{
+            System.out.println("Not equal");
+        }
+
+        if(s4 == s5){
             System.out.println("Equal");
         }
         else{
