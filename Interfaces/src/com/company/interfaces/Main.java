512b7e2bcb19c57a624af670dd547df94242539e
ikostan <igorkostan@gmail.com>
2018-01-10 21:53:44 -0800

    Working with interfaces

--- Interfaces/src/com/company/interfaces/Main.java
+++ Interfaces/src/com/company/interfaces/Main.java
@@ -4,6 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
+        try {
 
+            Person p1 = new Person("John", "Doe");
+            Person p2 = new Person("Bruce", "Lee", 30);
+
+            System.out.println(p1.compareTo(p2) < 0 ? p1.getFirstName() + " is younger" : p2.getFirstName() + " is younger");
+        }
+        catch (Exception e){
+
+            System.out.println(e.getMessage());
+        }
     }
 }
--- Interfaces/src/com/company/interfaces/Person.java
+++ Interfaces/src/com/company/interfaces/Person.java
@@ -3,8 +3,26 @@ package com.company.interfaces;
 public class Person implements Comparable<Person> {
 
     private String firstName;
-    private String LastName;
+    private String lastName;
     private int age;
+
+    public Person(){
+
+        this.age = 0;
+    }
+
+    public Person(String firstName, String lastName){
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Person(String firstName, String lastName, int age) throws Exception{
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        setAge(age);
+    }
 
     public String getFirstName() {
         return firstName;
@@ -15,19 +33,26 @@ public class Person implements Comparable<Person> {
     }
 
     public String getLastName() {
-        return LastName;
+        return lastName;
     }
 
     public void setLastName(String lastName) {
-        LastName = lastName;
+        this.lastName = lastName;
     }
 
     public int getAge() {
         return age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    public void setAge(int age) throws  Exception {
+
+        if(age >= 0){
+            this.age = age;
+        }
+        else{
+
+            throw new Exception("Age must be greater or equal to zero!");
+        }
     }
 
     @Override
