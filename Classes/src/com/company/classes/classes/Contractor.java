ad7f57b588eeabf82ec60f19761141476f70257e
ikostan <igorkostan@gmail.com>
2018-01-13 23:16:33 -0800

    Working with classes - class Contractor

---
+++ Classes/src/com/company/classes/classes/Contractor.java
@@ -0,0 +1,19 @@
+package com.company.classes.classes;
+
+/*
+* Worker that is not employed directly by the company
+*/
+public class Contractor extends Worker {
+
+    private Invoice[] invoices;
+
+    //Constructor
+    public Contractor(String name) {
+        super(name);
+    }
+
+    //Add a new (unpaid) invoice
+    public void attachInvoice(Invoice i){
+        //TODO
+    }
+}
---
+++ Classes/src/com/company/classes/classes/Employee.java
@@ -0,0 +1,16 @@
+package com.company.classes.classes;
+
+public class Employee extends Worker {
+
+    private Timesheet[] unpaidTimesheets;
+
+    //Constructor
+    public Employee(String name){
+        super(name);
+    }
+
+    //Add unpaid timesheet
+    public void attachTimesheet(Timesheet i){
+        //TODO
+    }
+}
