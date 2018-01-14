5695b86299cd27729e2bd11df2f7fa114d0f3c84
ikostan <igorkostan@gmail.com>
2018-01-13 23:06:55 -0800

    Working with classes - class Worker

---
+++ Classes/src/com/company/classes/classes/Worker.java
@@ -0,0 +1,16 @@
+package com.company.classes.classes;
+
+public class Worker {
+
+    //Class fields
+    private String name;
+
+    //Constructor
+    public Worker(String name){
+        this.name = name;
+    }
+
+    public void pay(){
+        //TODO
+    }
+}
