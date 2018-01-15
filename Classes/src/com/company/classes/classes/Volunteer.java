8f334103c73e9a5ee2127e45a14df0fc1fe33add
ikostan <igorkostan@gmail.com>
2018-01-14 18:20:12 -0800

    Working with classes - Using delegate in order to implement method

--- Classes/src/com/company/classes/classes/Volunteer.java
+++ Classes/src/com/company/classes/classes/Volunteer.java
@@ -3,8 +3,13 @@ package com.company.classes.classes;
 import com.company.classes.classes.contracts.Payable;
 
 public class Volunteer implements Payable {
+
+    //Using delegate
+    private Payable delegate = new Payable.Implementation();
+
+    //Using delegate in order to implement method
     @Override
     public void pay() {
-        //TODO
+        delegate.pay();
     }
 }
